82cd2d1661dc785fdcdbc69f9d569dff475df498
Chirag H Pandya <pandyac815@gmail.com>
2026-02-24 11:44:47 +0530

    Delete histogram.py

--- histogram.py
+++
@@ -1,8 +0,0 @@
-# define a procedure histogram() that takes a list of integer and print a histogram to the screen for example histogram([4,9,7]) should
-
-def histogram(number):
-    for n in number:
-        print("*" * n)
-
-#function call
-histogram([7,7,7])
