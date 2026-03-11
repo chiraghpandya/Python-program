52e43805b29f16aea3c157010f6a162227c8f32d
Chirag H Pandya <pandyac815@gmail.com>
2026-03-11 11:15:17 +0530

    work

--- prg2.1.py
+++ prg2.1.py
@@ -3,38 +3,43 @@
 subtraction, multiplication, and division.
  Call them based on user input.'''
 
-def add(num1,num2):
-    return num1+num2
-def sub(num1,num2):
-    return num1-num2
-def mul(num1,num2):
-    return num1*num2
-def div(num1,num2):
-    return num1/num2
-print("arithmetic operation:")
-print("1. Addition:")
-print("2. Subtraction:")
-print("3. Multiplication:")
-print("4. Division:")
+#Defining function
 
-num1 = float(input("Enter first number:"))
-num2 = float(input("Enter second number:"))
+def addition(a,b):
+    return a+b    
+def subtraction(a,b):
+    return a-b
+def multiplication(a,b):
+    return a*b
+def division(a,b):
+    if y == 0:
+        return "Error! Division by zero."
+    return x / y
 
-ch = int(input("Enter your choice (1-4):"))
+# Main program 
 
-if ch == 1:
-    result = add(num1,num2)
-elif ch == 2:
-    result = sub(num1,num2)
-elif ch == 3:
-    result = mul(num1,num2)
-elif ch == 4:
-    result = div(num1,num2)
+print("Select operation:") 
+print("1. Addition") 
+print("2. Subtraction") 
+print("3. Multiplication") 
+print("4. Division")
+
+#input from user
+choice = input("Enter choice (1/2/3/4): ") 
+num1 = float(input("Enter first number: ")) 
+num2 = float(input("Enter second number: "))
+
+#Main condition
+if choice == '1':
+    print(f"The result is: {addition(num1, num2)}")
+        
+elif choice == '2':
+    print(f"The result is: {subtraction(num1, num2)}")
+
+elif choice == '3':
+    print(f"The result is: {multiplication(num1, num2)}")
+     
+elif choice == '4':
+    print(f"The result is: {division(num1, num2)}")
 else:
-    result = "Invalid choice:"
-
-print("Result:",result)
-
-
-
-
+    print("Invalid input")
